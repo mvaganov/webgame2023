df3b1c92c826a720fe4619e8fc1140dfa29e1628
Michael Vaganov <mvaganov@hotmail.com>
2023-12-24 23:44:36 -0800

    Update Spreadsheet_Tooltip.cs

--- Assets/_MyGame/Scripts/Spreadsheet/Spreadsheet_Tooltip.cs
+++ Assets/_MyGame/Scripts/Spreadsheet/Spreadsheet_Tooltip.cs
@@ -13,66 +13,94 @@ namespace Spreadsheet {
 			}
 		}
 
+		public class MetaDataUiPair {
+			// TODO rename 'tooltip' to 'metadata'
+			public RectTransform tooltipElement;
+			public RectTransform tooltipAnchor;
+			public void RestartFade() {
+				Tooltip tooltip = tooltipElement.GetComponentInChildren<Tooltip>();
+				if (tooltip != null) {
+					tooltip.RestartFade();
+				}
+			}
+		}
+
 		private RectTransform _tooltipUiElement;
 		private RectTransform _tooltipAnchor;
 		private Dictionary<CellPosition, List<MetaData>> cellMetaData = new Dictionary<CellPosition, List<MetaData>>();
-		private Dictionary<CellPosition, RectTransform> cellMetaDataPopup = new Dictionary<CellPosition, RectTransform>();
+		private Dictionary<CellPosition, MetaDataUiPair> cellMetaDataPopup = new Dictionary<CellPosition, MetaDataUiPair>();
 
-		public void AddMetaData(CellPosition cellPosition, MetaData metaData) {
+		public MetaDataUiPair AddMetaData(CellPosition cellPosition, MetaData metaData) {
 			if (!cellMetaData.TryGetValue(cellPosition, out var metaList)) {
 				cellMetaData[cellPosition] = metaList = new List<MetaData> ();
 			}
 			metaList.Add(metaData);
+			return GetMetaDataPopup(cellPosition);
 		}
 
-		private RectTransform GetMetaDataPopup(CellPosition position) {
+		private MetaDataUiPair GetMetaDataPopup(CellPosition position) {
 			Cell cell = GetCellUi(position);
 			if (cell == null) {
 				return null;
 			}
-			if (!cellMetaDataPopup.TryGetValue(position, out RectTransform tooltipElement)) {
+			RectTransform cellRectTransform = cell.GetComponent<RectTransform>();
+			if (!cellMetaDataPopup.TryGetValue(position, out MetaDataUiPair metaDataPair)) {
+				cellMetaDataPopup[position] = metaDataPair = new MetaDataUiPair ();
 				Cell popup = cellGenerator.MakeNewCell(cellGenerator.PopupUiTypeIndex).Set(this, CellPosition.Invalid);
-				tooltipElement = popup.RectTransform;
-				RectTransform tooltipAnchor = new GameObject().AddComponent<RectTransform>();
-				tooltipAnchor.sizeDelta = Vector2.zero;
-				tooltipAnchor.anchorMin = tooltipElement.anchorMin;
-				tooltipAnchor.anchorMax = tooltipElement.anchorMax;
-				FollowRectTransform followRect = tooltipElement.gameObject.AddComponent<FollowRectTransform>();
-				followRect.toFollow = tooltipAnchor;
+				metaDataPair.tooltipElement = popup.RectTransform;
+				metaDataPair.tooltipAnchor = new GameObject().AddComponent<RectTransform>();
+				metaDataPair.tooltipAnchor.sizeDelta = Vector2.zero;
+				metaDataPair.tooltipAnchor.anchorMin = metaDataPair.tooltipElement.anchorMin;
+				metaDataPair.tooltipAnchor.anchorMax = metaDataPair.tooltipElement.anchorMax;
+				FollowRectTransform followRect = metaDataPair.tooltipElement.gameObject.AddComponent<FollowRectTransform>();
+				followRect.toFollow = metaDataPair.tooltipAnchor;
 			} else {
-				tooltipElement.SetAsLastSibling();
+				metaDataPair.tooltipElement.SetAsLastSibling();
 			}
 
-			return tooltipElement;
+			RectTransform popupRectTransform = metaDataPair.tooltipElement.GetComponent<RectTransform>();
+			cellMetaData.TryGetValue(position, out var metaList);
+			// TODO create some mechanism in the popup that allows it to scroll between the different meta datas
+			Ui.SetText(metaDataPair.tooltipElement, metaList[0].data);
+			Ui.SetColor(metaDataPair.tooltipElement, _tooltipColor);
+			popupRectTransform.SetParent(_transform.parent);
+			metaDataPair.tooltipAnchor.SetParent(ContentArea);
+			metaDataPair.tooltipAnchor.anchoredPosition = cellRectTransform.anchoredPosition
+				+ new Vector2(0, -cellRectTransform.sizeDelta.y);
+			metaDataPair.tooltipElement.gameObject.SetActive(true);
+			return metaDataPair;
 		}
 
 		public void SetMetaData(Cell cell, MetaData metaData) {
-			AddMetaData(cell.position, metaData);
-			if (_tooltipUiElement == null) {
-				Cell popup = cellGenerator.MakeNewCell(cellGenerator.PopupUiTypeIndex).Set(this, CellPosition.Invalid);
-				_tooltipUiElement = popup.RectTransform;
-				_tooltipAnchor = new GameObject().AddComponent<RectTransform>();
-				_tooltipAnchor.sizeDelta = Vector2.zero;
-				_tooltipAnchor.anchorMin = _tooltipUiElement.anchorMin;
-				_tooltipAnchor.anchorMax = _tooltipUiElement.anchorMax;
-				FollowRectTransform followRect = _tooltipUiElement.gameObject.AddComponent<FollowRectTransform>();
-				followRect.toFollow = _tooltipAnchor;
-			} else {
-				_tooltipUiElement.SetAsLastSibling();
-			}
-			RectTransform cellRectTransform = cell.GetComponent<RectTransform>();
-			RectTransform popupRectTransform = _tooltipUiElement.GetComponent<RectTransform>();
-			Ui.SetText(_tooltipUiElement, metaData.data);
-			Ui.SetColor(_tooltipUiElement, _tooltipColor);
-			popupRectTransform.SetParent(_transform.parent);
-			_tooltipAnchor.SetParent(ContentArea);
-			_tooltipAnchor.anchoredPosition = cellRectTransform.anchoredPosition
-				+ new Vector2(0, -cellRectTransform.sizeDelta.y);
-			_tooltipUiElement.gameObject.SetActive(true);
-			Tooltip tooltip = _tooltipUiElement.GetComponentInChildren<Tooltip>();
-			if (tooltip != null) {
-				tooltip.RestartFade();
-			}
+			MetaDataUiPair metaDataUi = AddMetaData(cell.position, metaData);
+			//RectTransform cellRectTransform = cell.GetComponent<RectTransform>();
+			//if (_tooltipUiElement == null) {
+			//	Cell popup = cellGenerator.MakeNewCell(cellGenerator.PopupUiTypeIndex).Set(this, CellPosition.Invalid);
+			//	_tooltipUiElement = popup.RectTransform;
+			//	_tooltipAnchor = new GameObject().AddComponent<RectTransform>();
+			//	_tooltipAnchor.sizeDelta = Vector2.zero;
+			//	_tooltipAnchor.anchorMin = _tooltipUiElement.anchorMin;
+			//	_tooltipAnchor.anchorMax = _tooltipUiElement.anchorMax;
+			//	FollowRectTransform followRect = _tooltipUiElement.gameObject.AddComponent<FollowRectTransform>();
+			//	followRect.toFollow = _tooltipAnchor;
+			//} else {
+			//	_tooltipUiElement.SetAsLastSibling();
+			//}
+			//RectTransform popupRectTransform = _tooltipUiElement.GetComponent<RectTransform>();
+			//Ui.SetText(_tooltipUiElement, metaData.data);
+			//Ui.SetColor(_tooltipUiElement, _tooltipColor);
+			//popupRectTransform.SetParent(_transform.parent);
+			//_tooltipAnchor.SetParent(ContentArea);
+			//_tooltipAnchor.anchoredPosition = cellRectTransform.anchoredPosition
+			//	+ new Vector2(0, -cellRectTransform.sizeDelta.y);
+			//_tooltipUiElement.gameObject.SetActive(true);
+			metaDataUi.RestartFade();
+			//Tooltip tooltip = _tooltipUiElement.GetComponentInChildren<Tooltip>();
+			//if (tooltip != null) {
+			//	tooltip.RestartFade();
+			//}
+
+			/// TODO add logic to clear the MetaDataUi correctly when it is scrolled out of view
 		}
 	}
 }
